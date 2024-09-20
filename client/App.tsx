6e3700f5ac57b23f2f3057267ba0c31f2456a753
Aochol <aocholayoul9@gmail.com>
2024-09-20 23:09:55 +0200

    working on repead mode

--- client/App.tsx
+++ client/App.tsx
@@ -4,55 +4,62 @@
  *
  * @format
  */
-/*
-
-
-export default App;*/
+/**/
 
 import React, { useEffect, useState } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Image } from 'react-native';
 import Slider from '@react-native-community/slider';
-import TrackPlayer, { Capability, State, useProgress } from 'react-native-track-player';
+import TrackPlayer, { Capability, State, useProgress, Event } from 'react-native-track-player';
 import Icon from 'react-native-vector-icons/Ionicons';
 
+
+// Define the type for each track in the queue
+/*type Track = {
+  id: number;
+  url: any;  // You can specify a more specific type if you know it (e.g., string for URLs)
+  title: string;
+  artist: string;
+  artwork: string;
+};
+*/
 const tracks = [
   {
-    id: '1',
-    url: require('./src/assests/song1.mp3'),
-    title: 'Track 1',
-    artist: 'Artist 1',
+    id: 1,
+    url: require('./src/assests/songs/FJULaFOI.mp3'),
+    title: 'La FOI',
+    artist: 'FJU LYON',
     artwork: 'https://images.unsplash.com/photo-1542379653-b928db1b4956?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=387&q=80',
   },
   {
-    id: '2',
-    url: require('./src/assests/song2.mp3'),
-    title: 'Track 2',
-    artist: 'Artist 2',
+    id: 2,
+    url: require('./src/assests/songs/FJUTConnaisPasLa.mp3'),
+    title: 'Tu Connais Pas La FJU',
+    artist: 'FJU LYON',
     artwork: 'https://media.istockphoto.com/id/1155368162/fr/photo/belle-jeune-femme-de-hipster-avec-les-cheveux-boucl%C3%A9s-avec-la-guitare-rouge-dans-les-lumi%C3%A8res.webp?a=1&b=1&s=612x612&w=0&k=20&c=xiL1Fkaj1FI2JKq8nVCJuOioG2WTLz_i9LxIUlXIdTQ=',
   },
   {
-    id: '3',
-    url: require('./src/assests/song3.mp3'),
-    title: 'Track 3',
-    artist: 'Artist 3',
+    id: 3,
+    url: require('./src/assests/songs/FJU-Reviensàlamaison.mp3'),
+    title: 'Reviens à maison',
+    artist: 'FJU LYON',
     artwork: 'https://media.istockphoto.com/id/1535648487/fr/photo/des-plans-rapproch%C3%A9s-dun-chanteur-attrayant-chantant-de-la-musique-et-jouant-de-la-guitare.webp?a=1&b=1&s=612x612&w=0&k=20&c=3ZS7sAGG6RXgJ4AQLu921mUILdKSjaoUgplFwSLTqvg=',
   },
   {
-    id: '4',
-    url: require('./src/assests/song4.mp3'),
-    title: 'Track 4',
-    artist: 'Artist 4',
+    id: 4,
+    url: require('./src/assests/songs/FJU-Parfum.mp3'),
+    title: 'Parfum',
+    artist: 'FJU LYON',
     artwork: 'https://media.istockphoto.com/id/607260652/fr/photo/trio-musical-cubain.webp?a=1&b=1&s=612x612&w=0&k=20&c=-dzpDI4Aw2pW91eBHFkpQugdC0A1jqNEsOo9glHvjbM=',
   },
   {
-    id: '5',
-    url: require('./src/assests/song5.mp3'),
-    title: 'Track 5',
-    artist: 'Artist 5',
+    id: 5,
+    url: require('./src/assests/songs/FJU-Lumière.mp3'),
+    title: 'Lumière',
+    artist: 'FJU LYON',
     artwork: 'https://media.istockphoto.com/id/1977233037/fr/photo/handsome-male-singer-on-stage.webp?a=1&b=1&s=612x612&w=0&k=20&c=Dhdk1gTGwfCUraxynuaZKs6fk2MQ3NJ3jTD9huQJUPQ=',
   },
   {
-    id: '6',
+    id: 6,
     url: require('./src/assests/song6.mp3'),
     title: 'Track 6',
     artist: 'Artist 6',
@@ -63,43 +70,57 @@ const tracks = [
 const App = () => {
   const progress = useProgress();
   const [isPlayerReady, setIsPlayerReady] = useState(false);
-  const [currentTrack, setCurrentTrack] = useState({ title: '', artist: '', artwork: '' });  // State to track if the player is ready
-
+  const [currentTrack, setCurrentTrack] = useState({ title: '', artist: '', artwork: 'https://media.istockphoto.com/id/607260652/fr/photo/trio-musical-cubain.webp?a=1&b=1&s=612x612&w=0&k=20&c=-dzpDI4Aw2pW91eBHFkpQugdC0A1jqNEsOo9glHvjbM=' });  // State to track if the player is ready
   useEffect(() => {
     const setupPlayer = async () => {
       try {
-        // Setup the player
-        await TrackPlayer.setupPlayer();
+          // Setup the player
+          await TrackPlayer.setupPlayer();
+          // Update options for the player (e.g., notifications, capabilities)
+          await TrackPlayer.updateOptions({
+            capabilities: [
+              Capability.Play,
+              Capability.Pause,
+              Capability.SkipToNext,
+              Capability.SkipToPrevious,
+              Capability.Stop,
+            ],
+            notificationCapabilities: [
+              Capability.Play,
+              Capability.Pause,
+              Capability.SkipToNext,
+              Capability.SkipToPrevious,
+              Capability.Stop,
+            ],
+            compactCapabilities: [
+              Capability.Play,
+              Capability.Pause,
+              Capability.SkipToNext,
+              Capability.SkipToPrevious,
+              Capability.Stop,
+            ],
+          });
 
-        // Update options for the player (e.g., notifications, capabilities)
-        await TrackPlayer.updateOptions({
-          capabilities: [
-            Capability.Play,
-            Capability.Pause,
-            Capability.SkipToNext,
-            Capability.SkipToPrevious,
-            Capability.Stop,
-          ],
-        });
-
-        // Add a track to the player queue
-        await TrackPlayer.add(tracks);
-
-        // Play the first track automatically
-       // await TrackPlayer.play();
-
-        // Update current track info when the player starts
-        updateCurrentTrackInfo();
-
-        // Mark the player as ready
-        setIsPlayerReady(true);
+          // Add a track to the player queue
+          await TrackPlayer.add(tracks);
+          // Play the first track automatically
+           await TrackPlayer.play();
+          // Event listener for when the playback queue ends
+           TrackPlayer.addEventListener(Event.PlaybackQueueEnded, async () => {
+            console.log('Queue ended, skipping back to the first track...');
+            await TrackPlayer.seekTo(0);  // Seek to the start of the first track
+            await TrackPlayer.play();     // Play the first track
+          });
+          // Update current track info when the player starts
+          updateCurrentTrackInfo();
+          // Mark the player as ready
+          setIsPlayerReady(true);
       } catch (error) {
         console.error('Error setting up TrackPlayer:', error);
       }
     };
 
     setupPlayer();
-
     // Cleanup on component unmount
     return () => {
       TrackPlayer.reset();
