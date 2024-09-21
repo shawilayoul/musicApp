82033aee65a8d1a4b91d9980e44dd479b37a26d0
Aochol <aocholayoul9@gmail.com>
2024-09-21 17:59:44 +0200

    updating content of the track

---
+++ client/src/app/playList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+const playList = () => {
+    return (
+        <View>
+            <Text>playList</Text>
+        </View>
+    )
+}
+
+export default playList
--- client/src/hooks/useSetupTrackPlayer.ts
+++ client/src/hooks/useSetupTrackPlayer.ts
@@ -1,8 +1,14 @@
-import { SetStateAction, useEffect } from 'react';
-import TrackPlayer, { Capability,Event } from 'react-native-track-player';
+import {SetStateAction, useEffect} from 'react';
+import TrackPlayer, {Capability, Event} from 'react-native-track-player';
 import tracks from '../assests/data/track';
 
-export const useSetUpTrackPlayer = ( updateCurrentTrackInfo: { (): Promise<void>; (): void; }, setIsPlayerReady: { (value: SetStateAction<boolean>): void; (arg0: boolean): void; }) => {
+export const useSetUpTrackPlayer = (
+  updateCurrentTrackInfo: {(): Promise<void>; (): void},
+  setIsPlayerReady: {
+    (value: SetStateAction<boolean>): void;
+    (arg0: boolean): void;
+  },
+) => {
   useEffect(() => {
     const setupPlayer = async () => {
       try {
@@ -58,9 +64,21 @@ export const useSetUpTrackPlayer = ( updateCurrentTrackInfo: { (): Promise<void>
 
     setupPlayer();
     // Cleanup on component unmount
+    // Listen for when the track changes
+    const trackChangeListener = TrackPlayer.addEventListener(
+      Event.PlaybackActiveTrackChanged,
+      async data => {
+        if (data.track !== null) {
+          // Check if the current track exists
+          // Update the track info when the track changes
+          updateCurrentTrackInfo();
+        }
+      },
+    );
     return () => {
+      trackChangeListener.remove();
       TrackPlayer.reset();
     };
-  // eslint-disable-next-line react-hooks/exhaustive-deps
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 };
